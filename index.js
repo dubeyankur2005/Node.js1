9dab34e021f6da0d8738e1922ca7325a7522e606
dubeyankur2005 <dubeyankur6961>
2026-05-03 10:44:32 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("npm folder working");
+
+setTimeout(() =>{
+    console.log("Time out Executed");
+},8000);
+
+console.log("I am the end");
+
+
+
+
+
+
+
+
+
+console.log(" 1st :- synchronous task");
+
+let sum = 0;
+for(let a = 0; a <=90000000000000; a++){
+    sum += a;
+}
+console.log("2nd :-",sum);
+
+console.log("3rd :- end of synchronous task");
+
